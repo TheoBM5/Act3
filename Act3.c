8a4e7d787e189d23567a8e26beccf932e8826dc7
Teodoro Barba <teodoro.barba4022@alumnos.udg.mx>
2020-09-19 23:44:12 -0500

    Se agrego la funcion fgets para poder leer el nombre completo

--- Act3.c
+++ Act3.c
@@ -4,7 +4,7 @@ int main(){
     char nombre[100];
     printf("Hola mundo\n");
     printf("Nombre: ");
-    scanf("%s", nombre);
+    fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s", nombre);
     return 0;
 }
