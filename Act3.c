2e085046c6880a2369b7005285d2236268dff37d
Teodoro Barba <teodoro.barba4022@alumnos.udg.mx>
2020-09-19 23:41:31 -0500

    Se capturo el nombre e imprimimos un saludo

--- Act3.c
+++ Act3.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
 int main(){
-
+    char nombre[100];
     printf("Hola mundo\n");
-    
+    printf("Nombre: ");
+    scanf("%s", nombre);
+    printf("Hola %s", nombre);
     return 0;
 }
